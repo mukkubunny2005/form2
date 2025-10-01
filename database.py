167d5e668175a796486281369f17645e816d4ed2
MUKKU BUNNY <mukkubunny2005@gmail.com>
2025-10-01 16:19:35 +0530

    Updated database.py with corrected schema

--- database.py
+++ database.py
@@ -2,18 +2,12 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-# ----------------------------
-# Helper function
-# ----------------------------
 def get_database_url(env_var, default_url):
     return os.getenv(env_var, default_url)
 
-# ----------------------------
-# DB URLs
-# ----------------------------
 SQLALCHEMY_DATABASE_URL = get_database_url(
-    "DATABASE_URL",  # Render env variable
-    "mysql+pymysql://root:bunny123@localhost:3306/hostel_form"  # Local fallback
+    "DATABASE_URL",  # Render env
+    "mysql+pymysql://root:bunny123@localhost:3306/hostel_form"  # local dev
 )
 
 SQLALCHEMY_DATABASE_URL2 = get_database_url(
@@ -26,23 +20,14 @@ SQLALCHEMY_DATABASE_URL3 = get_database_url(
     "mysql+pymysql://root:bunny123@localhost:3306/login_form"
 )
 
-# ----------------------------
-# Engines
-# ----------------------------
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 engine2 = create_engine(SQLALCHEMY_DATABASE_URL2)
 engine3 = create_engine(SQLALCHEMY_DATABASE_URL3)
 
-# ----------------------------
-# Sessions
-# ----------------------------
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 SessionLocal2 = sessionmaker(autocommit=False, autoflush=False, bind=engine2)
 SessionLocal3 = sessionmaker(autocommit=False, autoflush=False, bind=engine3)
 
-# ----------------------------
-# Base Models
-# ----------------------------
 Base = declarative_base()
 Base2 = declarative_base()
 Base3 = declarative_base()
