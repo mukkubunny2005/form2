2642a7cda22bf6a2faa1610daf34f634b4810b07
MUKKU BUNNY <mukkubunny2005@gmail.com>
2025-10-01 16:10:30 +0530

    Updated database.py with corrected schema

--- database.py
+++ database.py
@@ -1,18 +1,48 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:bunny123@localhost:3306/hostel_form')
-SQLALCHEMY_DATABASE_URL2 = os.getenv('DATABASE_URL', 'mysql+pymysql://root:bunny123@localhost:3306/tenant')
-SQLALCHEMY_DATABASE_URL3 = os.getenv('DATABASE_URL', 'mysql+pymysql://root:bunny123@localhost:3306/login_form')
+# ----------------------------
+# Helper function
+# ----------------------------
+def get_database_url(env_var, default_url):
+    return os.getenv(env_var, default_url)
+
+# ----------------------------
+# DB URLs
+# ----------------------------
+SQLALCHEMY_DATABASE_URL = get_database_url(
+    "DATABASE_URL",  # Render env variable
+    "mysql+pymysql://root:bunny123@localhost:3306/hostel_form"  # Local fallback
+)
+
+SQLALCHEMY_DATABASE_URL2 = get_database_url(
+    "DATABASE_URL2",
+    "mysql+pymysql://root:bunny123@localhost:3306/tenant"
+)
+
+SQLALCHEMY_DATABASE_URL3 = get_database_url(
+    "DATABASE_URL3",
+    "mysql+pymysql://root:bunny123@localhost:3306/login_form"
+)
+
+# ----------------------------
+# Engines
+# ----------------------------
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 engine2 = create_engine(SQLALCHEMY_DATABASE_URL2)
 engine3 = create_engine(SQLALCHEMY_DATABASE_URL3)
+
+# ----------------------------
+# Sessions
+# ----------------------------
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 SessionLocal2 = sessionmaker(autocommit=False, autoflush=False, bind=engine2)
 SessionLocal3 = sessionmaker(autocommit=False, autoflush=False, bind=engine3)
+
+# ----------------------------
+# Base Models
+# ----------------------------
 Base = declarative_base()
 Base2 = declarative_base()
 Base3 = declarative_base()
-
